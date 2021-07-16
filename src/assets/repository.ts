14565c8af62d13df29172286fbb4d6bb4b887205
JeremyYu <JeremyYu@chatlabs.com>
2021-07-16 10:25:43 +0800

    add taro-react-kit-ts template

--- src/assets/repository.ts
+++ src/assets/repository.ts
@@ -1,4 +1,6 @@
 export default {
   'taro-react-kit-2.x':
     'direct:https://bitbucket.org/JeremyYu_1122/taro-react-kit-2.x/get/master.zip',
+  'taro-react-kit-ts-3.x':
+    'direct:https://bitbucket.org/JeremyYu_1122/taro-react-kit-ts-3.x/get/master.zip',
 };
