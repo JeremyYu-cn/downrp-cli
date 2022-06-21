705dda3e81da92d25a58f4773956cd27f60724ab
IchliebedichZhu <IchliebedichZhu@gmail.com>
2022-06-21 10:22:16 +0800

    feat: add 2 new modules

--- src/assets/repository.ts
+++ src/assets/repository.ts
@@ -3,4 +3,8 @@ export default {
     'direct:https://bitbucket.org/JeremyYu_1122/taro-react-kit-2.x/get/master.zip',
   'taro-react-kit-ts-3.x':
     'direct:https://bitbucket.org/JeremyYu_1122/taro-react-kit-ts-3.x/get/master.zip',
+  'react-background-ts':
+    'direct:https://gitee.com/IchliebedichZhu/common-template/repository/blazearchive/react-background-ts.zip',
+  'node-server-ts':
+    'direct:https://gitee.com/IchliebedichZhu/common-template/repository/blazearchive/node-server-ts.zip',
 };
