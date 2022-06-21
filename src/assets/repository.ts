087c423a13598e7ccd11b4ef12e656b5b0a6cea5
IchliebedichZhu <IchliebedichZhu@gmail.com>
2022-06-21 13:58:48 +0800

    fix: change repository brand and url

--- src/assets/repository.ts
+++ src/assets/repository.ts
@@ -4,7 +4,7 @@ export default {
   'taro-react-kit-ts-3.x':
     'direct:https://bitbucket.org/JeremyYu_1122/taro-react-kit-ts-3.x/get/master.zip',
   'react-background-ts':
-    'direct:https://gitee.com/IchliebedichZhu/common-template/repository/blazearchive/react-background-ts.zip',
+    'direct:https://bitbucket.org/jeremy_1995/commontemplate/get/e1a1b3354ef2ec0eda0eda2ee7a45e6b6f2b4dfd.zip',
   'node-server-ts':
-    'direct:https://gitee.com/IchliebedichZhu/common-template/repository/blazearchive/node-server-ts.zip',
+    'direct:https://bitbucket.org/jeremy_1995/commontemplate/get/32f524ea14cb4a480f3ca3bdb1caf56f7fac6e67.zip',
 };
